d233a9f58f1610a612667e7c3639afac8eaa6dbe
Anonymous <>
2026-05-30 20:30:48 +0200

    LAVA_WAAM - dataset restructuring

---
+++ run/representativeness_LAVA_WAAM_released.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from helpers.utils import parse_files
+
+if __name__ == "__main__":
+    parse_files(
+        base_dir=Path(
+            "dataset_representativeness/LAVA_WAAM__released"
+        )
+    )
